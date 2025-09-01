da833a1bfbdb9a6c503de784cfcdbecb17551cd4
Harlem Kirua <debabel1hackamani@gmail.com>
2025-09-01 15:06:36 +0000

    edited

--- backend/moteur.js
+++ backend/moteur.js
@@ -3,9 +3,8 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const session = require("express-session");
 
-
-const classroomsRoutes =  require("./routes/classroomsRoutes");
-const ecolesRoutes =  require("./routes/ecolesRoutes");
+const classroomsRoutes = require("./routes/classroomsRoutes");
+const ecolesRoutes = require("./routes/ecolesRoutes");
 const rolesRoutes = require("./routes/rolesRoutes");
 const utilisateurRoutes = require("./routes/utilisateursRoutes");
 const apprenantsRoutes = require("./routes/apprenantsRoutes");
@@ -16,32 +15,41 @@ const scolaritesRoutes = require("./routes/scolaritesRoutes");
 const presencesRoutes = require("./routes/presencesRoutes");
 const notesRoutes = require("./routes/notesRoutes");
 
-
 const app = express();
 
-app.use(cors({
-  origin: "http://localhost:5173",
-  credentials: true
-}));
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
-app.use(session({
-  secret: "475mmmpmkk",   
-  resave: false,
-  saveUninitialized: false,
-  cookie: { secure: false }
-}));
+app.use(
+  session({
+    secret: "475mmmpmkk",
+    resave: false,
+    saveUninitialized: false,
+    cookie: { secure: false },
+  })
+);
 
+const path = require("path");
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 
 app.use(bodyParser.json());
 app.use("/uploads", express.static("public/uploads"));
 
 // Import des routes
-app.use("/api/roles", rolesRoutes );
-app.use("/api/ecoles", ecolesRoutes );
+app.use("/api/roles", rolesRoutes);
+app.use("/api/ecoles", ecolesRoutes);
 app.use("/api/apprenants", apprenantsRoutes);
-app.use("/api/classrooms", classroomsRoutes );
-app.use("/api/utilisateurs", utilisateurRoutes );
-app.use("/api/utilisadestroy", utilisateurRoutes );
+app.use("/api/classrooms", classroomsRoutes);
+app.use("/api/utilisateurs", utilisateurRoutes);
+app.use("/api/utilisadestroy", utilisateurRoutes);
 app.use("/api/sanctions", sanctionsRoutes);
 app.use("/api/conduites", conduitesRoutes);
 app.use("/api/matieres", matieresRoutes);
@@ -49,20 +57,7 @@ app.use("/api/scolarites", scolaritesRoutes);
 app.use("/api/presences", presencesRoutes);
 app.use("/api/notes", notesRoutes);
 
-
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`🚀 Serveur vitrine_ecole lancé sur ${PORT}`);
 });
-
-
-
-
-
-
-
-
-
-
-
-
